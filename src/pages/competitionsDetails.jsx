16f4e75a0c18347f70cae218bf6fa009b669b0d2
Dhanidayo <sarahjoseph@ymail.com>
2022-01-18 16:22:10 +0100

    created switch tabs

--- src/pages/competitionsDetails.jsx
+++ src/pages/competitionsDetails.jsx
@@ -1,10 +1,39 @@
 import { useState } from "react";
+import '../styling/leagues.css';
 
 const CompetitionDetails = () => {
     const [currentlyViewing, setCurrentlyViewing] = useState('standings');
+
+    const viewSection = (section) => {
+		setCurrentlyViewing(section)
+	}
+
     
     return (
-        <h1>hi</h1>
+        <>
+            <div className="competition-tables">
+                <ul className="switch-tab">
+                    <li onClick={()=> viewSection("standings")} exact activeClassname="active">Standings</li>
+                    <li onClick={()=> viewSection("matches")} exact activeClassname="active">Matches</li>
+                </ul>
+            </div>
+            {currentlyViewing === "standings" &&
+                <section>
+                    <div className="standings">
+                        <h1>This is the standings tab.</h1>
+                    </div>
+                </section>
+            }
+            {currentlyViewing === "matches" &&
+                <section>
+                    <div className="matches">
+                        <h1>
+                            This is the matches tab.
+                        </h1>
+                    </div>
+                </section>
+            }
+        </>
     );
 }
  
